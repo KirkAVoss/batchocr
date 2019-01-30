3fc42e3460b5473b86458bf251a704c48d78adc0
KV <kirk.voss@gmail.com>
2019-01-30 15:49:21 -0600

    Added a few edits

--- batchocr.py
+++ batchocr.py
@@ -8,4 +8,6 @@ parser.add_argument('file', type=argparse.FileType('r'), nargs='+')
 args = parser.parse_args()
 
 for file in args.file:
-    call(['ocrmypdf',str(file),'OCRed_' + str(file)])
+    #print(file,str(file),file.name)
+    print("Processing:",file.name)
+    call(['ocrmypdf','-q',str(file.name),'OCRed_' + str(file.name)])
